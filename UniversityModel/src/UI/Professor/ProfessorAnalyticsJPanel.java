a6903c63013fbf68923b6c25d6e5d68f092e9607
emily <wang.shig@northeastern.edu>
2023-03-21 17:56:07 -0400

    added data performance table for professor

--- UniversityModel/src/UI/Professor/ProfessorAnalyticsJPanel.java
+++ UniversityModel/src/UI/Professor/ProfessorAnalyticsJPanel.java
@@ -4,9 +4,16 @@
  */
 package UI.Professor;
 
+import CourseCatalog.Course;
+import CourseCatalog.CourseOffer;
+import CourseCatalog.CourseSchedule;
+import CourseCatalog.Seat;
 import Platform.Platform;
 import Professor.Professor;
 import UserAccount.UserAccount;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -21,6 +28,7 @@ public class ProfessorAnalyticsJPanel extends javax.swing.JPanel {
     private Platform platform;
     private UserAccount ua;
     private Professor professor;
+    private DefaultTableModel dataTableModel;
     
     public ProfessorAnalyticsJPanel(Platform platform, UserAccount useraccount) {
         initComponents();
@@ -28,13 +36,55 @@ public class ProfessorAnalyticsJPanel extends javax.swing.JPanel {
         this.platform = platform;
         this.ua = useraccount;
         this.professor = this.platform.getProfessorDirectory().findProfessorById(ua.getAccountId());
+        this.dataTableModel = (DefaultTableModel) dataTable.getModel();
         
         fieldRevenue.setText(String.valueOf(this.professor.getTuitionCollected()));
         fieldStudents.setText(String.valueOf(this.professor.getEnrolledListForAllTerm().size()));
+        
+        populateTable();
     }
     
     public ProfessorAnalyticsJPanel() {
         initComponents();
+    }
+    
+    public void populateTable(){
+        dataTableModel.setRowCount(0);
+    if (!this.professor.getAllSchedules().entrySet().isEmpty()){
+        
+        //get data
+        for (Course course: this.professor.getCourseCatalog().getCourses()){
+            String courseName = course.getName();
+            int registedCountForCourse = course.getRegistedCountForCourse();
+            int passCountForCourse = course.getPassCountForCourse();
+            int revenueForCourse = course.getRevenueForCourse();
+            
+            for (Map.Entry<String,CourseSchedule> termSchedule: this.professor.getAllSchedules().entrySet()){
+                CourseSchedule cs = termSchedule.getValue();
+                ArrayList<CourseOffer> offers = cs.getSchedule();
+                
+                for (CourseOffer co: offers){
+                    if (co.getCourse().getName().equals(courseName)){
+                        registedCountForCourse += co.getEnrolledStudentList().size();
+                        revenueForCourse += co.getTotalCourseRevenues();
+                        passCountForCourse += co.getPassCount();
+                    }
+                }
+            }
+        }
+        
+        //populate data into table
+        for (Course c: this.professor.getCourseCatalog().getCourses()){
+            Object[] row = new Object[4];
+            
+            row[0] = c.getName();
+            row[1] = c.getRegistedCountForCourse();
+            row[2] = c.getPassCountForCourse()/c.getRegistedCountForCourse()*100 + "%";
+            row[3] = c.getRevenueForCourse();
+            
+            dataTableModel.addRow(row);
+        }
+    }
     }
 
     /**
@@ -52,6 +102,9 @@ public class ProfessorAnalyticsJPanel extends javax.swing.JPanel {
         fieldRevenue = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         fieldStudents = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        dataTable = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -76,45 +129,80 @@ public class ProfessorAnalyticsJPanel extends javax.swing.JPanel {
 
         fieldStudents.setEditable(false);
 
+        dataTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Course name", "Student count", "Pass rate", "Revenue"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(dataTable);
+
+        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
+        jLabel2.setText("My dashboard");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(58, 58, 58)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(fieldRevenue, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane2)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(45, 45, 45)
+                                .addComponent(fieldRevenue, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(49, 49, 49)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(fieldStudents, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(fieldStudents, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(171, Short.MAX_VALUE))
+                        .addGap(320, 320, 320)
+                        .addComponent(jLabel2)))
+                .addContainerGap(90, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(56, 56, 56)
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(fieldRevenue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(16, 16, 16)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(fieldRevenue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(fieldStudents, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
-                .addContainerGap(182, Short.MAX_VALUE))
+                .addGap(29, 29, 29)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 259, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(80, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable dataTable;
     private javax.swing.JTextField fieldRevenue;
     private javax.swing.JTextField fieldStudents;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
